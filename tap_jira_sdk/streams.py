0e6f7ad222627b431c04b90a4efb21cc815f81d6
NeilGorman104 <neil@ryan-miranda.com>
2023-05-17 16:44:19 -0400

    Add UserStream, FieldStream, and ServerInfoStream Inline Schemas

--- tap_jira_sdk/streams.py
+++ tap_jira_sdk/streams.py
@@ -8,58 +8,217 @@ from singer_sdk import typing as th  # JSON Schema typing helpers
 
 from tap_jira_sdk.client import tap-jira-sdkStream
 
-# TODO: Delete this is if not using json files for schema definition
-SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
-# TODO: - Override `UsersStream` and `GroupsStream` with your own stream definition.
-#       - Copy-paste as many times as needed to create multiple stream types.
-
+PropertiesList = th.PropertiesList
+Property = th.Property
+ObjectType = th.ObjectType
+DateTimeType = th.DateTimeType
+StringType = th.StringType
+ArrayType = th.ArrayType
+BooleanType = th.BooleanType
+IntegerType = th.IntegerType
 
 class UsersStream(tap-jira-sdkStream):
     """Define custom stream."""
 
-    name = "users"
-    path = "/users"
-    primary_keys = ["id"]
-    replication_key = None
-    # Optionally, you may also use `schema_filepath` in place of `schema`:
-    # schema_filepath = SCHEMAS_DIR / "users.json"  # noqa: ERA001
-    schema = th.PropertiesList(
-        th.Property("name", th.StringType),
-        th.Property(
-            "id",
-            th.StringType,
-            description="The user's system ID",
-        ),
-        th.Property(
-            "age",
-            th.IntegerType,
-            description="The user's age in years",
-        ),
-        th.Property(
-            "email",
-            th.StringType,
-            description="The user's email address",
-        ),
-        th.Property("street", th.StringType),
-        th.Property("city", th.StringType),
-        th.Property(
-            "state",
-            th.StringType,
-            description="State name in ISO 3166-2 format",
-        ),
-        th.Property("zip", th.StringType),
+    columns = """
+                 Id
+              """
+
+    name = "user"
+    path = "/user?accountId=62851352222d36006fb739dc"
+    primary_keys = ["Id"]
+    replication_key = "LastModifiedDate"
+    replication_method = "incremental"
+
+    schema = PropertiesList(
+        Property("accountId", StringType),
+
+
     ).to_dict()
 
+    def get_url_params(
+            self,
+            context: dict | None,  # noqa: ARG002
+            next_page_token: Any | None,
+    ) -> dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization.
 
-class GroupsStream(tap-jira-sdkStream):
+        Args:
+            context: The stream context.
+            next_page_token: The next page index or value.
+
+        Returns:
+            A dictionary of URL query parameters.
+        """
+        params: dict = {}
+        if next_page_token:
+            params["page"] = next_page_token
+        if self.replication_key:
+            params["sort"] = "asc"
+            params["order_by"] = self.replication_key
+
+        return params
+
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+        """Parse the response and return an iterator of result records.
+
+        Args:
+            response: The HTTP ``requests.Response`` object.
+
+        Yields:
+            Each record from the source.
+        """
+
+        resp_json = response.json()
+
+        if isinstance(resp_json, list):
+            results = resp_json
+        elif resp_json.get("records") is not None:
+            results = resp_json["records"]
+        else:
+            results = resp_json
+
+        yield from results
+
+class FieldStream(tap-jira-sdkStream):
     """Define custom stream."""
 
-    name = "groups"
-    path = "/groups"
-    primary_keys = ["id"]
-    replication_key = "modified"
-    schema = th.PropertiesList(
-        th.Property("name", th.StringType),
-        th.Property("id", th.StringType),
-        th.Property("modified", th.DateTimeType),
+    columns = """
+                 id, key, name, custom, orderable, navigate, searchable, clauseNames, schema
+              """
+
+    name = "field"
+    path = "/field"
+    primary_keys = ["Id"]
+    replication_key = "LastModifiedDate"
+    replication_method = "incremental"
+
+    schema = PropertiesList(
+        Property("id", StringType),
+        Property("key", StringType),
+        Property("name", StringType),
+        Property("custom", StringType),
+        Property("orderable", StringType),
+        Property("navigate", StringType),
+        Property("searchable", StringType),
+        Property("clauseNames", StringType),
+        Property("schema", StringType),
+
+
     ).to_dict()
+
+    def get_url_params(
+            self,
+            context: dict | None,  # noqa: ARG002
+            next_page_token: Any | None,
+    ) -> dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization.
+
+        Args:
+            context: The stream context.
+            next_page_token: The next page index or value.
+
+        Returns:
+            A dictionary of URL query parameters.
+        """
+        params: dict = {}
+        if next_page_token:
+            params["page"] = next_page_token
+        if self.replication_key:
+            params["sort"] = "asc"
+            params["order_by"] = self.replication_key
+
+        return params
+
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+        """Parse the response and return an iterator of result records.
+
+        Args:
+            response: The HTTP ``requests.Response`` object.
+
+        Yields:
+            Each record from the source.
+        """
+
+        resp_json = response.json()
+
+        if isinstance(resp_json, list):
+            results = resp_json
+        elif resp_json.get("records") is not None:
+            results = resp_json["records"]
+        else:
+            results = resp_json
+
+        yield from results
+class ServerInfoStream(tap-jira-sdkStream):
+    """Define custom stream."""
+
+    columns = """
+                 baseUrl, version, versionNumbers, deploymentType, buildNumber, buildDate, serverTime, scmInfo, serverTitle, defaultLocale
+              """
+
+    name = "serverInfo"
+    path = "/serverInfo"
+    primary_keys = ["Id"]
+    replication_key = "LastModifiedDate"
+    replication_method = "incremental"
+
+    schema = PropertiesList(
+        Property("baseUrl", StringType),
+        Property("version", StringType),
+        Property("versionNumbers", StringType),
+        Property("deploymentType", StringType),
+        Property("buildNumber", StringType),
+        Property("buildDate", StringType),
+        Property("serverTime", StringType),
+        Property("scmInfo", StringType),
+        Property("serverTitle", StringType),
+        Property("defaultLocale", StringType),
+
+
+
+    ).to_dict()
+
+    def get_url_params(
+            self,
+            context: dict | None,  # noqa: ARG002
+            next_page_token: Any | None,
+    ) -> dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization.
+
+        Args:
+            context: The stream context.
+            next_page_token: The next page index or value.
+
+        Returns:
+            A dictionary of URL query parameters.
+        """
+        params: dict = {}
+        if next_page_token:
+            params["page"] = next_page_token
+        if self.replication_key:
+            params["sort"] = "asc"
+            params["order_by"] = self.replication_key
+
+        return params
+
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+        """Parse the response and return an iterator of result records.
+
+        Args:
+            response: The HTTP ``requests.Response`` object.
+
+        Yields:
+            Each record from the source.
+        """
+
+        resp_json = response.json()
+
+        if isinstance(resp_json, list):
+            results = resp_json
+        elif resp_json.get("records") is not None:
+            results = resp_json["records"]
+        else:
+            results = resp_json
+
+        yield from results
