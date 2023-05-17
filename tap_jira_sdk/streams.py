ce1faf595f1ce24dc97d76a1a5efa7912649b794
NeilGorman104 <neil@ryan-miranda.com>
2023-05-17 19:19:42 -0400

    add  IssueType, Project, Status streams

--- tap_jira_sdk/streams.py
+++ tap_jira_sdk/streams.py
@@ -21,17 +21,27 @@ class UsersStream(tap-jira-sdkStream):
     """Define custom stream."""
 
     columns = """
-                 Id
+                 self, accountId, accountType, avatarUrls, displayName, active, timeZone, locale, groups, applicationRoles, expand
               """
 
     name = "user"
     path = "/user?accountId=62851352222d36006fb739dc"
-    primary_keys = ["Id"]
-    replication_key = "LastModifiedDate"
+    primary_keys = ["accountId"]
+    replication_key = "accountId"
     replication_method = "incremental"
 
     schema = PropertiesList(
+        Property("self", StringType),
         Property("accountId", StringType),
+        Property("accountType", StringType),
+        Property("avatarUrls", StringType),
+        Property("displayName", StringType),
+        Property("active", StringType),
+        Property("timeZone", StringType),
+        Property("locale", StringType),
+        Property("groups", StringType),
+        Property("applicationRoles", StringType),
+        Property("expand", StringType),
 
 
     ).to_dict()
@@ -84,12 +94,12 @@ class FieldStream(tap-jira-sdkStream):
     """Define custom stream."""
 
     columns = """
-                 id, key, name, custom, orderable, navigate, searchable, clauseNames, schema
+                 id, key, name, untranslatedName, custom, orderable, navigate, searchable, clauseNames, schema
               """
 
     name = "field"
     path = "/field"
-    primary_keys = ["Id"]
+    primary_keys = ["id"]
     replication_key = "LastModifiedDate"
     replication_method = "incremental"
 
@@ -97,6 +107,7 @@ class FieldStream(tap-jira-sdkStream):
         Property("id", StringType),
         Property("key", StringType),
         Property("name", StringType),
+        Property("untranslatedname", StringType),
         Property("custom", StringType),
         Property("orderable", StringType),
         Property("navigate", StringType),
@@ -159,8 +170,8 @@ class ServerInfoStream(tap-jira-sdkStream):
 
     name = "serverInfo"
     path = "/serverInfo"
-    primary_keys = ["Id"]
-    replication_key = "LastModifiedDate"
+    primary_keys = ["baseUrl"]
+    replication_key = "serverTime"
     replication_method = "incremental"
 
     schema = PropertiesList(
@@ -174,6 +185,228 @@ class ServerInfoStream(tap-jira-sdkStream):
         Property("scmInfo", StringType),
         Property("serverTitle", StringType),
         Property("defaultLocale", StringType),
+
+
+
+    ).to_dict()
+
+    def get_url_params(
+            self,
+            context: dict | None,  # noqa: ARG002
+            next_page_token: Any | None,
+    ) -> dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization.
+
+        Args:
+            context: The stream context.
+            next_page_token: The next page index or value.
+
+        Returns:
+            A dictionary of URL query parameters.
+        """
+        params: dict = {}
+        if next_page_token:
+            params["page"] = next_page_token
+        if self.replication_key:
+            params["sort"] = "asc"
+            params["order_by"] = self.replication_key
+
+        return params
+
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+        """Parse the response and return an iterator of result records.
+
+        Args:
+            response: The HTTP ``requests.Response`` object.
+
+        Yields:
+            Each record from the source.
+        """
+
+        resp_json = response.json()
+
+        if isinstance(resp_json, list):
+            results = resp_json
+        elif resp_json.get("records") is not None:
+            results = resp_json["records"]
+        else:
+            results = resp_json
+
+        yield from results
+
+class IssueTypeStream(tap-jira-sdkStream):
+    """Define custom stream."""
+
+    columns = """
+                 self, id, description, iconUrl, name, untranslatedName, subtask, avatarId, hierarchyLevel, scope
+              """
+
+    name = "IssueType"
+    path = "/issuetype"
+    primary_keys = ["id"]
+    replication_key = "self"
+    replication_method = "incremental"
+
+    schema = PropertiesList(
+        Property("self", StringType),
+        Property("id", StringType),
+        Property("description", StringType),
+        Property("iconUrl", StringType),
+        Property("name", StringType),
+        Property("untranslatedName", StringType),
+        Property("subtask", StringType),
+        Property("avatarId", StringType),
+        Property("hierarchyLevel", StringType),
+        Property("scope", StringType),
+
+
+
+    ).to_dict()
+
+    def get_url_params(
+            self,
+            context: dict | None,  # noqa: ARG002
+            next_page_token: Any | None,
+    ) -> dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization.
+
+        Args:
+            context: The stream context.
+            next_page_token: The next page index or value.
+
+        Returns:
+            A dictionary of URL query parameters.
+        """
+        params: dict = {}
+        if next_page_token:
+            params["page"] = next_page_token
+        if self.replication_key:
+            params["sort"] = "asc"
+            params["order_by"] = self.replication_key
+
+        return params
+
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+        """Parse the response and return an iterator of result records.
+
+        Args:
+            response: The HTTP ``requests.Response`` object.
+
+        Yields:
+            Each record from the source.
+        """
+
+        resp_json = response.json()
+
+        if isinstance(resp_json, list):
+            results = resp_json
+        elif resp_json.get("records") is not None:
+            results = resp_json["records"]
+        else:
+            results = resp_json
+
+        yield from results
+
+
+class StatusStream(tap-jira-sdkStream):
+    """Define custom stream."""
+
+    columns = """
+                 self, description, iconUrl, name, untranslatedName, id, statusCategory, scope
+              """
+
+    name = "Status"
+    path = "/status"
+    primary_keys = ["id"]
+    replication_key = "self"
+    replication_method = "incremental"
+
+    schema = PropertiesList(
+        Property("self", StringType),
+        Property("description", StringType),
+        Property("iconUrl", StringType),
+        Property("name", StringType),
+        Property("untranslatedName", StringType),
+        Property("id", StringType),
+        Property("statusCategory", StringType)
+        Property("scope", StringType),
+
+
+
+    ).to_dict()
+
+    def get_url_params(
+            self,
+            context: dict | None,  # noqa: ARG002
+            next_page_token: Any | None,
+    ) -> dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization.
+
+        Args:
+            context: The stream context.
+            next_page_token: The next page index or value.
+
+        Returns:
+            A dictionary of URL query parameters.
+        """
+        params: dict = {}
+        if next_page_token:
+            params["page"] = next_page_token
+        if self.replication_key:
+            params["sort"] = "asc"
+            params["order_by"] = self.replication_key
+
+        return params
+
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+        """Parse the response and return an iterator of result records.
+
+        Args:
+            response: The HTTP ``requests.Response`` object.
+
+        Yields:
+            Each record from the source.
+        """
+
+        resp_json = response.json()
+
+        if isinstance(resp_json, list):
+            results = resp_json
+        elif resp_json.get("records") is not None:
+            results = resp_json["records"]
+        else:
+            results = resp_json
+
+        yield from results
+
+
+class ProjectStream(tap-jira-sdkStream):
+    """Define custom stream."""
+
+    columns = """
+                 expand, self, id, key, name, avatarUrls, projectTypeKey, simplified, style, isPrivate, properties, entityId, uuid
+              """
+
+    name = "Project"
+    path = "/project"
+    primary_keys = ["id"]
+    replication_key = "self"
+    replication_method = "incremental"
+
+    schema = PropertiesList(
+        Property("expand", StringType),
+        Property("self", StringType),
+        Property("id", StringType),
+        Property("key", StringType),
+        Property("name", StringType),
+        Property("avatarUrls", StringType),
+        Property("projectTypeKey", StringType),
+        Property("simplified", StringType)
+        Property("style", StringType),
+        Property("isPrivate", StringType),
+        Property("properties", StringType),
+        Property("entityId", StringType),
+        Property("uuid", StringType),
 
 
 
