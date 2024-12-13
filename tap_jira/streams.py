5669da0f639cf1a1d2519c8c1b23e9126fc7cc6a
Jeff George <jeff@eb0.org>
2024-12-13 16:26:20 -0500

    Quote the start_date / end_date params for future handling of date parsing

--- tap_jira/streams.py
+++ tap_jira/streams.py
@@ -1631,11 +1631,11 @@ class IssueStream(JiraStream):
 
         if "start_date" in self.config:
             start_date = self.config["start_date"]
-            params["jql"].append(f"(created>={start_date} or updated>={start_date})")
+            params["jql"].append(f"(created>='{start_date}' or updated>='{start_date}')")
 
         if "end_date" in self.config:
             end_date = self.config["end_date"]
-            params["jql"].append(f"(created<{end_date} or updated<{end_date})")
+            params["jql"].append(f"(created<'{end_date}' or updated<'{end_date}')")
 
         if params["jql"]:
             jql = " and ".join(params["jql"])
