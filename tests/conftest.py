651531ce2221314bd6463cc15cd6ed4b644542b2
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2024-12-14 13:02:48 -0600

    Add available env vars to pytest report header

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,20 @@
 """Test Configuration."""
+
+from __future__ import annotations
+
+import os
+import typing as t
+
+if t.TYPE_CHECKING:
+    import pathlib
+
+    import pytest
+
+
+def pytest_report_header(
+    config: pytest.Config,  # noqa: ARG001
+    start_path: pathlib.Path,  # noqa: ARG001
+) -> list[str] | str:
+    """Add a header to the test report."""
+    tap_jira_vars = [var for var in os.environ if var.startswith("TAP_JIRA")]
+    return f"tap-jira environment variables: {tap_jira_vars}"
