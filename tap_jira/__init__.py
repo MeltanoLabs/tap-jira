ee8dbe3aeb39df03ddee90f0ddeff7b476ac906e
Reuben <60552974+ReubenFrankel@users.noreply.github.com>
2026-02-16 15:16:28 +0000

    Patch `fromisoformat` for Python versions before 3.11 only
    Co-authored-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>

--- tap_jira/__init__.py
+++ tap_jira/__init__.py
@@ -1,5 +1,8 @@
 """Tap for tap-jira."""
 
-from backports.datetime_fromisoformat import MonkeyPatch
+import sys
 
-MonkeyPatch.patch_fromisoformat()
+if sys.version_info < (3, 11):
+    from backports.datetime_fromisoformat import MonkeyPatch
+
+    MonkeyPatch.patch_fromisoformat()
