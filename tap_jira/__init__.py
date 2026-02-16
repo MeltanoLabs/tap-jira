de3fcfc072177d373e8ad16abc65922a8e683fff
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2026-02-16 09:20:02 -0600

    Apply suggestion from @edgarrmondragon

--- tap_jira/__init__.py
+++ tap_jira/__init__.py
@@ -3,6 +3,6 @@
 import sys
 
 if sys.version_info < (3, 11):
-    from backports.datetime_fromisoformat import MonkeyPatch
+    from backports.datetime_fromisoformat import MonkeyPatch  # ty: ignore[unresolved-import]
 
     MonkeyPatch.patch_fromisoformat()
