e9d3782e7c72b71b6f7d133568dcf1a2650aeff4
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2026-06-02 18:02:11 -0600

    test: Handle third-party warnings programatically
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import os
+import sys
 import typing as t
 
 if t.TYPE_CHECKING:
@@ -18,3 +19,12 @@ def pytest_report_header(
     """Add a header to the test report."""
     tap_jira_vars = [var for var in os.environ if var.startswith("TAP_JIRA")]
     return f"tap-jira environment variables: {tap_jira_vars}"
+
+
+def pytest_configure(config: pytest.Config) -> None:
+    """Configure pytest."""
+    if sys.version_info < (3, 11):
+        config.addinivalue_line(
+            "filterwarnings",
+            "once:Python 3.10 will reach its end of life on 2026-10:FutureWarning",
+        )
